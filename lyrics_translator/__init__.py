55b3ecc9141cbd5c4f7d1906ddd406d297c50e21
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2022-11-05 18:43:10 +0100

    fix version

--- lyrics_translator/__init__.py
+++ lyrics_translator/__init__.py
@@ -1,4 +1,12 @@
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
 
 from lyrics_translator.core.lyrics_translator import LyricsTranslator
 from lyrics_translator.core.utils import create_folder, get_base_path, read_songs
