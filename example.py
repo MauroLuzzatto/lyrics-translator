f548dd3d38eb9a8fee7415fa38593747ff85a6ed
Mauro Luzzatto <mauroluzzatto@hotmail.com>
2023-01-16 21:49:16 +0100

    update file

--- example.py
+++ example.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     artist = "The Beach Boys"
 
     for language in ["de", "sv"]:
-
         translator = LyricsTranslator(language)
         lyrics = translator.get_song_translation(song, artist)
         lyrics.save()
